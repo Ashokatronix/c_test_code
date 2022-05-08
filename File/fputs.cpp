10893fc09d958e8f0ee12d77740f887c1c328755
ashokatronix <ashokatronix@gmail.com>
2022-05-08 23:08:48 +0530

    fputs added

---
+++ File/fputs.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include<stdlib.h>
+
+int main(void)
+{
+    FILE *fptr;
+    char str[80];
+    if((fptr = fopen("test", "w")) == NULL)
+    {
+        printf("Error in opening file\n");
+        exit(1);
+    }
+
+    printf("Enter the text\n");
+    printf("To stop entering, press ctrl+d/ctrl+z\n");
+    while(gets(str)!=NULL)
+    {
+        fputs(str,fptr);
+    }
+    fclose(fptr);
+    return 0;
+}
