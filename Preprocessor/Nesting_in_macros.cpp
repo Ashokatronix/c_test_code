067ea37968ecf63bd40532b711bc7d12ae5e439a
ashokatronix <ashokatronix@gmail.com>
2022-05-29 15:14:28 +0530

    Preprocessor

---
+++ Preprocessor/Nesting_in_macros.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#define ISLOWER(c) (c>=97 && c<=122)
+#define ISUPPER(c) (c>=65 && c<=90)
+#define ISALPHA(c) ISLOWER(c)||ISUPPER(c)
+#define ISNUM(c)   (c>=48 && c<=57)
+#define ISALPHANUM(c) ISALPHA(c) || ISNUM(c)
+
+int main()
+{
+    char ch;
+    printf("Enter a char: \n");
+    scanf("%c",&ch);
+    if(ISALPHANUM(ch))
+        printf("%c is an alphanumeric char\n",ch);
+    else
+        printf("%c is not an alphanumeric char\n",ch);
+    return 0;
+}
